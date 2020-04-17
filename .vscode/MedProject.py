cfa4d94410490dfddc52ec0f19c01c7f7e327f17
Danny Sweeney <danny@Dannys-MacBook-Pro.local>
2020-04-17 13:46:28 -0400

    First Commit

---
+++ .vscode/MedProject.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('/Users/danny/Downloads/Inpatient_Prospective_Payment_System__IPPS__Provider_Summary_for_the_Top_100_Diagnosis-Related_Groups__DRG__-_FY2011.csv', dtype = str)
+print("Hello World")
